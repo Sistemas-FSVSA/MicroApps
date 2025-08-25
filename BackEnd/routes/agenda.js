7365f45277a8dc394bb87f8b022e52186170a2ba
Sistemas-FSVSA <sistemas@funerariasanvicente.com>
2025-08-25 11:04:30 -0500

    ajuste funcionalidades backend

--- BackEnd/routes/agenda.js
+++ BackEnd/routes/agenda.js
@@ -8,17 +8,14 @@ const { uploadFirma, uploadFields } = require('../models/multer');
 
 const { crearReservacion } = require('../controllers/agenda/crearReservacion');
 const { getReservaciones } = require('../controllers/agenda/getReservaciones');
-const { limpiarReservacionesExpiradas } = require('../controllers/agenda/limpiarReservacionesExpiradas');
-const { nuevaReservacion } = require('../controllers/agenda/nuevaReservacion');
 const { listarDependencias } = require('../controllers/agenda/listarDependencias');
 const { obtenerDependenciaPorId } = require('../controllers/agenda/obtenerDependenciaPorId');
 
 router.post('/crearReservacion', rateLimiterFast, crearReservacion);
 router.get('/getReservaciones', rateLimiterFast, getReservaciones);
-router.post('/limpiarReservaciones', rateLimiterStrict, limpiarReservacionesExpiradas);
-router.post('/nuevaReservacion', rateLimiterFast, nuevaReservacion);
 router.get('/listarDependencias', rateLimiterFast, listarDependencias);
-router.get('/obtenerDependencia/:id', rateLimiterFast, obtenerDependenciaPorId);
+router.get('/dependencia/:id', rateLimiterFast, obtenerDependenciaPorId);
+
 
 
 module.exports = router;
